d83c960968917fbea650c9a442ccda181d2aa206
Chaika813 <orysiya.ch@gmail.com>
2020-02-28 21:32:01 +0200

    FIrst Unicorn

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CensucTractsConverter from './components/censucTractsConverter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CensucTractsConverter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/censucTractsConverter.js
@@ -0,0 +1,67 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class CencucTractsConverter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.convert = this.convert.bind(this);
+        //  this.handleFirstInputChange = this.handleFirstInputChange.bind(this);
+        this.state = {
+            inputOneText: '',
+            inputTwoText: []
+        }
+    }
+
+    handleFirstInputChange = (event) => {
+        this.setState ({
+            inputOneText: event.target.value
+        })
+    }
+
+    convert() {
+        // 1. save input to variable
+        const needToConvert = this.state.inputOneText 
+        // 2. split text by comma
+        const splitedInput = needToConvert.split(", ")
+        // 3. you'll have array, trim each value so there is no spaces before and after - trim()
+        
+        // 4. for each value add ' before and after
+         for (let i=0; i < splitedInput.length; i++) {
+         splitedInput[i]= '\''+ splitedInput[i] + '\''
+          }
+         
+        // // // 5. join all values with comma
+         const joinedInput = splitedInput.join()
+        // 6. display it in second text area
+         console.log(joinedInput)
+        this.setState ({
+            inputTwoText: joinedInput
+        });
+    }
+
+// make textareas wide and big
+
+    render() {
+        return (
+            <>
+            {/* <div class="bg" ></div> */}
+                <h1>Census tracts convert for QGIS</h1>
+                <div class="container-fluid">
+                <div > 
+                    <textarea class="wide-large-border"  id="area1" onChange={this.handleFirstInputChange}/>
+                </div>
+                <button class="btn btn-primary " onClick={this.convert} ><i class="fa fa-hand-spock-o"></i>Convert</button>
+                <div>
+                    <textarea  class="wide-large-border"  id="area2" value={this.state.inputTwoText}/>
+                </div>
+                </div>
+            </>
+        
+            
+        );}
+}
+
+
+export default CencucTractsConverter;
